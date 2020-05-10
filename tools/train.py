28538ba0460cf5cbcf9f5f8948867c3ad2c1ca1d
hwang <53857938+hellohwang@users.noreply.github.com>
2020-05-10 23:42:05 +0800

    add

--- tools/train.py
+++ tools/train.py
@@ -3,7 +3,7 @@ from tensorflow.keras import datasets, layers, models
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
 train_dir = "../datasets/data/clothes_dataset/train"
-val_dir = ""
+val_dir = "../datasets/data/clothes_dataset/val"
 BATCH_SIZE = 8
 
 NUM_GPUS = 2
@@ -41,14 +41,65 @@ ds_train = tf.data.Dataset.list_files(train_dir) \
   .shuffle(buffer_size=1000).batch(BATCH_SIZE) \
   .prefetch(tf.data.experimental.AUTOTUNE)
 
-def schedule(epoch):
-  initial_learning_rate = BASE_LEARNING_RATE * BS_PER_GPU / 128
-  learning_rate = initial_learning_rate
-  for mult, start_epoch in LR_SCHEDULE:
-    if epoch >= start_epoch:
-      learning_rate = initial_learning_rate * mult
-    else:
-      break
-  tf.summary.scalar('learning rate', data=learning_rate, step=epoch)
-  return learning_rate
+ds_valid = tf.data.Dataset.list_files(train_dir) \
+  .map(load_image, num_parallel_calls=tf.data.experimental.AUTOTUNE) \
+  .shuffle(buffer_size=1000).batch(BATCH_SIZE) \
+  .prefetch(tf.data.experimental.AUTOTUNE)
 
+optimizer = optimizers.Adam()
+loss_func = losses.MeanSquaredError()
+
+train_loss = tf.keras.metrics.Mean(name='train_loss')
+train_metric = tf.keras.metrics.MeanAbsoluteError(name='train_mae')
+
+valid_loss = tf.keras.metrics.Mean(name='valid_loss')
+valid_metric = tf.keras.metrics.MeanAbsoluteError(name='valid_mae')
+
+
+@tf.function
+def train_step(model, features, labels):
+  with tf.GradientTape() as tape:
+    predictions = model(features)
+    loss = loss_func(labels, predictions)
+  gradients = tape.gradient(loss, model.trainable_variables)
+  optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+
+  train_loss.update_state(loss)
+  train_metric.update_state(labels, predictions)
+
+
+@tf.function
+def valid_step(model, features, labels):
+  predictions = model(features)
+  batch_loss = loss_func(labels, predictions)
+  valid_loss.update_state(batch_loss)
+  valid_metric.update_state(labels, predictions)
+
+
+@tf.function
+def train_model(model, ds_train, ds_valid, epochs):
+  for epoch in tf.range(1, epochs + 1):
+    for features, labels in ds_train:
+      train_step(model, features, labels)
+
+    for features, labels in ds_valid:
+      valid_step(model, features, labels)
+
+    logs = 'Epoch={},Loss:{},MAE:{},Valid Loss:{},Valid MAE:{}'
+
+    if epoch % 100 == 0:
+      # printbar()
+      tf.print(tf.strings.format(logs,
+                                 (epoch, train_loss.result(), train_metric.result(), valid_loss.result(),
+                                  valid_metric.result())))
+      tf.print("w=", model.layers[0].kernel)
+      tf.print("b=", model.layers[0].bias)
+      tf.print("")
+
+    train_loss.reset_states()
+    valid_loss.reset_states()
+    train_metric.reset_states()
+    valid_metric.reset_states()
+
+
+train_model(model, ds_train, ds_valid, 400)
